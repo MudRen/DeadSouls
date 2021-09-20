2fcb60e99e25eff7c3f7598c0fc1366720904ffe
mudren <i@Mud.ren>
2021-09-20 23:07:47 +0800

    chmod 755 to 644

--- lib/secure/include/secrets.h
+++ lib/secure/include/secrets.h
@@ -4,8 +4,8 @@
 #define IRN_PASSWORD1            "Bravo"
 #define IRN_PASSWORD2            "Charlie"
 #define IRN_PASSWORD3            "Delta"
-#define IMC2_CLIENT_PW           "clientpass"
-#define IMC2_SERVER_PW           "serverpass"
-#define I3_SERVER_PW             1234567890
+#define IMC2_CLIENT_PW "jcaeegkllc"
+#define IMC2_SERVER_PW "mihicgjjhm"
+#define I3_SERVER_PW 116300422
 #define INSTANCE_PW              "1234567890"
 #endif /* s_secrets_h */
